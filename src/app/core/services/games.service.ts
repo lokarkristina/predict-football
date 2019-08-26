bfcecdec038154bdf2e0b9dfd7576ac9c34c748e
Kristina Davidovic <kristina.davidovic@agiledrop.com>
2019-08-26 08:29:45 +0200

    Add fetchCountry method to the service.

--- src/app/core/services/games.service.ts
+++ src/app/core/services/games.service.ts
@@ -5,29 +5,37 @@ import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { Game } from '../../games/models/game.model';
+import { Country } from 'src/app/games/models/country.model';
 
 @Injectable({ providedIn: 'root' })
 export class GamesService {
-  url = 'http://localhost:3000/predictions';
+  urlPredictions = 'http://localhost:3000/predictions';
+  urlCountries = 'http://localhost:3000/countries';
 
   constructor(private http: HttpClient) {}
 
   fetchGames(): Observable<Game[]> {
     return this.http
-      .get<Game[]>(this.url)
+      .get<Game[]>(this.urlPredictions)
       .pipe(catchError((error: any) => Observable.throw(error.json())));
   }
 
   fetchGame(id: number): Observable<Game> {
     return this.http
-      .get<Game>(`${this.url}/${id}`)
+      .get<Game>(`${this.urlPredictions}/${id}`)
+      .pipe(catchError((error: any) => Observable.throw(error.json())));
+  }
+
+  getCountry(id: number) {
+    return this.http
+      .get<Country>(`${this.urlCountries}/${id}`)
       .pipe(catchError((error: any) => Observable.throw(error.json())));
   }
 
   // Add = update v resnice, ker updatea Game s tem ko mu doda še score
   addPrediction(payload: Game): Observable<Game> {
     return this.http
-      .put<Game>(`${this.url}/${payload.id}`, payload)
+      .put<Game>(`${this.urlPredictions}/${payload.id}`, payload)
       .pipe(catchError((error: any) => Observable.throw(error.json())));
   }
 }
