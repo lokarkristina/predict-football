134e679d34fbf43bbcf3ed989ca486b7747a2e5f
Kristina Davidovic <kristina.davidovic@agiledrop.com>
2019-09-19 12:10:02 +0200

    Add games filtering by prediction added or not.

--- src/app/games/containers/games-container/games.component.ts
+++ src/app/games/containers/games-container/games.component.ts
@@ -13,10 +13,9 @@ import { Game } from '../../models/game.model';
   styleUrls: ['./games.component.scss'],
 })
 export class GamesComponent implements OnInit {
+  constructor(private store: Store<fromStore.GameState>) {}
   games$: Observable<Game[]>;
   breakpoint: number;
-
-  constructor(private store: Store<fromStore.GameState>) {}
 
   ngOnInit() {
     this.games$ = this.store.select(fromStore.getAllGames);
@@ -39,5 +38,52 @@ export class GamesComponent implements OnInit {
         : window.innerWidth <= 991
         ? 3
         : 4;
+  }
+
+  onFilterSelection(filterClass: string, event) {
+    const gameElements = document.getElementsByClassName('game-details');
+
+    if (filterClass == 'all') {
+      filterClass = '';
+    }
+
+    for (let i = 0; i < gameElements.length; i++) {
+      this.removeClass(gameElements[i], 'show');
+      gameElements[i].className.indexOf(filterClass) > -1 &&
+        this.addClass(gameElements[i], 'show');
+    }
+
+    const btns = document.getElementsByClassName('filter-button');
+    const current = event.target;
+
+    for (let i = 0; i < btns.length; i++) {
+      this.removeClass(btns[i], 'active');
+    }
+
+    this.addClass(current.parentNode, 'active');
+  }
+
+  private removeClass(element, name: string) {
+    const arr1 = element.className.split(' ');
+    const arr2 = name.split(' ');
+
+    for (let i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);
+      }
+    }
+
+    element.className = arr1.join(' ');
+  }
+
+  private addClass(element, name: string) {
+    const arr1 = element.className.split(' ');
+    const arr2 = name.split(' ');
+
+    for (let i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {
+        element.className += ' ' + arr2[i];
+      }
+    }
   }
 }
